7d61fed82f69d203bf62c1d2fab3110f10f1d09b
petecallaghan <wpete.callaghan@gmail.com>
2018-11-16 12:40:11 +0000

    Generated code by SwaggerHub

--- nodejs-server/service/PublicService.js
+++ nodejs-server/service/PublicService.js
@@ -16,68 +16,70 @@
  * roadCategory String filter by Road Category (optional)
  * startJunction String filter by Start Junction (optional)
  * endJunction String filter by End Junction (optional)
- * returns List
+ * returns JunctionLinks
  **/
 exports.queryJunctions = function(skip,limit,aADFYear,cP,estimation_method,region,localAuthority,road,roadCategory,startJunction,endJunction) {
   return new Promise(function(resolve, reject) {
     var examples = {};
-    examples['application/json'] = [ {
-  "LinkLength_km" : 6.7,
-  "Estimation_method" : "Counted",
-  "AllHGVs" : 7004,
-  "PedalCycles" : 0,
-  "LightGoodsVehicles" : 4851,
-  "AADFYear" : 2000,
-  "V2AxleRigidHGV" : 1854,
-  "V6orMoreAxleArticHGV" : 1630,
-  "Northing" : 112172,
-  "LinkLength_miles" : 4.16,
-  "V3AxleRigidHGV" : 398,
-  "CarsTaxis" : 41341,
-  "LocalAuthority" : "Devon",
-  "RoadCategory" : "TM",
-  "Road" : "M5",
-  "CP" : 6023,
-  "AllMotorVehicles" : 53717,
-  "V5AxleArticHGV" : 1935,
-  "Estimation_method_detailed" : "Manual count",
-  "StartJunction" : "28",
-  "BusesCoaches" : 384,
-  "EndJunction" : "27",
-  "Motorcycles" : 137,
-  "V3or4AxleArticHGV" : 1014,
-  "Region" : "South West",
-  "Easting" : 303700,
-  "V4or5AxleRigidHGV" : 173
-}, {
-  "LinkLength_km" : 6.7,
-  "Estimation_method" : "Counted",
-  "AllHGVs" : 7004,
-  "PedalCycles" : 0,
-  "LightGoodsVehicles" : 4851,
-  "AADFYear" : 2000,
-  "V2AxleRigidHGV" : 1854,
-  "V6orMoreAxleArticHGV" : 1630,
-  "Northing" : 112172,
-  "LinkLength_miles" : 4.16,
-  "V3AxleRigidHGV" : 398,
-  "CarsTaxis" : 41341,
-  "LocalAuthority" : "Devon",
-  "RoadCategory" : "TM",
-  "Road" : "M5",
-  "CP" : 6023,
-  "AllMotorVehicles" : 53717,
-  "V5AxleArticHGV" : 1935,
-  "Estimation_method_detailed" : "Manual count",
-  "StartJunction" : "28",
-  "BusesCoaches" : 384,
-  "EndJunction" : "27",
-  "Motorcycles" : 137,
-  "V3or4AxleArticHGV" : 1014,
-  "Region" : "South West",
-  "Easting" : 303700,
-  "V4or5AxleRigidHGV" : 173
-} ];
+    examples['application/json'] = {
+  "JunctionLinks" : [ {
+    "LinkLength_km" : 6.7,
+    "Estimation_method" : "Counted",
+    "AllHGVs" : 7004,
+    "PedalCycles" : 0,
+    "LightGoodsVehicles" : 4851,
+    "AADFYear" : 2000,
+    "V2AxleRigidHGV" : 1854,
+    "V6orMoreAxleArticHGV" : 1630,
+    "Northing" : 112172,
+    "LinkLength_miles" : 4.16,
+    "V3AxleRigidHGV" : 398,
+    "CarsTaxis" : 41341,
+    "LocalAuthority" : "Devon",
+    "RoadCategory" : "TM",
+    "Road" : "M5",
+    "CP" : 6023,
+    "AllMotorVehicles" : 53717,
+    "V5AxleArticHGV" : 1935,
+    "Estimation_method_detailed" : "Manual count",
+    "StartJunction" : "28",
+    "BusesCoaches" : 384,
+    "EndJunction" : "27",
+    "Motorcycles" : 137,
+    "V3or4AxleArticHGV" : 1014,
+    "Region" : "South West",
+    "Easting" : 303700,
+    "V4or5AxleRigidHGV" : 173
+  }, {
+    "LinkLength_km" : 6.7,
+    "Estimation_method" : "Counted",
+    "AllHGVs" : 7004,
+    "PedalCycles" : 0,
+    "LightGoodsVehicles" : 4851,
+    "AADFYear" : 2000,
+    "V2AxleRigidHGV" : 1854,
+    "V6orMoreAxleArticHGV" : 1630,
+    "Northing" : 112172,
+    "LinkLength_miles" : 4.16,
+    "V3AxleRigidHGV" : 398,
+    "CarsTaxis" : 41341,
+    "LocalAuthority" : "Devon",
+    "RoadCategory" : "TM",
+    "Road" : "M5",
+    "CP" : 6023,
+    "AllMotorVehicles" : 53717,
+    "V5AxleArticHGV" : 1935,
+    "Estimation_method_detailed" : "Manual count",
+    "StartJunction" : "28",
+    "BusesCoaches" : 384,
+    "EndJunction" : "27",
+    "Motorcycles" : 137,
+    "V3or4AxleArticHGV" : 1014,
+    "Region" : "South West",
+    "Easting" : 303700,
+    "V4or5AxleRigidHGV" : 173
+  } ]
+};
     if (Object.keys(examples).length > 0) {
       resolve(examples[Object.keys(examples)[0]]);
     } else {
